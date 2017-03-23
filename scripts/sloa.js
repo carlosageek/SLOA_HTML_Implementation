20943f185ad19da69a7bbb44c6509bf673b1428d
jsymphor <symphorienj@gmail.com>
2017-03-23 10:04:46 -0400

    yo

--- scripts/sloa.js
+++ scripts/sloa.js
@@ -18,3 +18,27 @@ $(function() {
         html:true
     });
 });
+
+var image1 = new Image("/images/book1.jpeg");
+var image2 = new Image();
+image2.src = "/images/book2.jpeg";
+var image3 = new Image();
+image3.src = "/images/book3.jpeg";
+var image4 = new Image();
+image4.src = "/images/book4.jpeg";
+var image5 = new Image();
+image5.src = "/images/book5.jpeg";
+
+var step = 1;
+function slideIt(){
+
+    document.images.slide.src = eval("image"+step+".src");
+    if (step < 5)
+        step++;
+    else {
+        step = 1;
+
+    }
+    setTimeout("slideIt()",1500)
+}
+slideIt();
