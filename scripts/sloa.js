4e4d287255fb7b471fbae35bd397609c54b27889
Carlos Estrada <cestrad2@dtcc.edu>
2017-03-27 19:23:46 -0400

    few changes

--- scripts/sloa.js
+++ scripts/sloa.js
@@ -11,34 +11,36 @@ function closeNav() {
     document.getElementById("mySideNav").style.width = "0";
 }
 
+
 $(function() {
     $('#no-live').popover();
     $(document).popover({
         selector: '[data-toggle="popover"]',
         html:true
     });
+    $('#search').popover();
+    $(document).popover({
+        selector: '[data-toggle="popover"]',
+        html:true
+    });
 });
 
-var image1 = new Image("/images/book1.jpeg");
-var image2 = new Image();
-image2.src = "/images/book2.jpeg";
-var image3 = new Image();
-image3.src = "/images/book3.jpeg";
-var image4 = new Image();
-image4.src = "/images/book4.jpeg";
-var image5 = new Image();
-image5.src = "/images/book5.jpeg";
+var images = [];
+images[0] = "/images/book1.jpeg";
+images[1] = "/images/book2.jpeg";
+images[2] = "/images/book3.jpeg";
+images[3] = "/images/book4.jpeg";
+images[4] = "/images/book5.jpeg";
 
 var step = 1;
+//noinspection InfiniteRecursionJS
 function slideIt(){
 
-    document.images.slide.src = eval("image"+step+".src");
-    if (step < 5)
+    document.getElementById("slide").src = images[step];
+    if (step < images.length-1)
         step++;
     else {
-        step = 1;
-
+        step = 0;
     }
-    setTimeout("slideIt()",1500)
 }
-slideIt();
+window.onload=setInterval(slideIt(), 1500);
